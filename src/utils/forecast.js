d69d513895926a353545490b6ca756da9152270b
Norm Draper <normdraper@gmail.com>
2020-12-29 20:05:11 -0600

    Wind speed added.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,5 @@
 const request = require('request')
 
-// const url = 'http://api.weatherstack.com/current?access_key=83320d5478c294317b1ee7b693cadb9d&query=37.8267,-122.4233&units=f'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log(error)
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//         console.log(response.body.error)
-//     } else {
-//         const current = response.body.current
-//         // console.log(current)
-//         console.log('The temperature is currently '+current.temperature+' but it feels like '+current.feelslike)
-//     }
-// })
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=83320d5478c294317b1ee7b693cadb9d&query='+latitude+','+longitude+'&units=f'
     // console.log('forecasturl: ', url)
@@ -26,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error, undefined)
         } else {
             callback(undefined,
-                 'The temperature is currently '+body.current.temperature+' but it feels like '+body.current.feelslike
+                 'The temperature is currently '+body.current.temperature+' but it feels like '+body.current.feelslike+'. The wind is from the '+body.current.wind_dir+' at '+body.current.wind_speed+' MPH.'
             //     {
             //     temperature: response.body.current.temperature,
             //     feelslike: response.body.current.feelslike
